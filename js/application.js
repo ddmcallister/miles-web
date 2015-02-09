f624c65cf25f091b7d4209bd9e196f76489fe82a
ddmcallister <destanie@gmail.com>
2015-02-08 22:17:00 -0500

    playing w co drops circular nav not there yet
    starts to work, then makes a donut?

--- js/application.js
+++ js/application.js
@@ -1,6 +1,55 @@
-window.onload = function() {
-	alert("hey hey");
-	var note = document.getElementById("test_note");
-	TweenLite.to(note, 2, {rotation:"-360", transformOrigin:"0% 100%"});
+/*window.onload = function() {
 
- }
+    var width, height, largeHeader, canvas, ctx, target, animateHeader = true;
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: 0, y: height};
+
+        largeHeader = document.getElementById('large-header');
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById("trumpet-canvas");
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+
+        }
+}
+
+	var note = document.getElementById("test_note");
+    var notes = [
+        {url: "../img/Music-dotnote.svg"},
+        {url: "../img/Music-sixteenthnote.svg"},
+        {url: "../img/Music-sixteenthnote.svg"},
+        {url: "../img/Music-sixteenthrest.svg"},
+        ];
+
+    for (i=0; i<notes.length; i++) {
+        note.background = notes(i);
+    }*/
+
+function main() {
+var open = false;
+
+$(".cn-button").click(function() {
+    $(".cn-wrapper").toggleClass('opened-nav');
+
+    if(!open) {
+      this.innerHTML = "Close";
+    }
+    else{
+      this.innerHTML = "Menu";
+    }
+    open = !open;
+
+    function closeWrapper(){
+    $(".cn-wrapper").removeClass('opened-nav');
+  }
+});
+
+}
+
+$(document).ready(main);
