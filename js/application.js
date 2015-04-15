6fa96f602c3b37f79c935f5dfb7628d53c0cb6fc
ddmcallister <destanie@gmail.com>
2015-04-15 00:01:47 -0400

    playing with codrops nav demo
    finally found the typo …

--- js/application.js
+++ js/application.js
@@ -1,42 +1,9 @@
-/*window.onload = function() {
-
-    var width, height, largeHeader, canvas, ctx, target, animateHeader = true;
-
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = {x: 0, y: height};
-
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
-
-        canvas = document.getElementById("trumpet-canvas");
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
-
-
-        }
-}
-
-	var note = document.getElementById("test_note");
-    var notes = [
-        {url: "../img/Music-dotnote.svg"},
-        {url: "../img/Music-sixteenthnote.svg"},
-        {url: "../img/Music-sixteenthnote.svg"},
-        {url: "../img/Music-sixteenthrest.svg"},
-        ];
-
-    for (i=0; i<notes.length; i++) {
-        note.background = notes(i);
-    }*/
-
 function main() {
+
 var open = false;
 
-$(".cn-button").click(function() {
-    $(".cn-wrapper").toggleClass('opened-nav');
-
+$("#cn-button").click(function() {
+    $("div#cn-wrapper").toggleClass("opened-nav");
     if(!open) {
       this.innerHTML = "Close";
     }
